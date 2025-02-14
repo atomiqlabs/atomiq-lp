cac3230c9309a48bf00348ccd588dd960956e116
adambor <adamborteam@gmail.com>
2025-02-14 17:46:01 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@ async function main() {
     const chains: {[chainId: string]: ChainData & {commands?: Command<any>[]}} = {};
     const registeredChains: {[chainId: string]: ChainInitializer<any, any, any>} = RegisteredChains;
     for(let chainId in registeredChains) {
-        if(registeredChains[chainId]==null) continue;
+        if(IntermediaryConfig[chainId]==null) continue;
         chains[chainId] = {
             ...registeredChains[chainId].loadChain(IntermediaryConfig[chainId], bitcoinRpc),
             allowedTokens: allowedTokens[chainId] ?? [],
