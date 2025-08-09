c6c026d85f924855e9576f5889ae058bad4a0736
adambor <adamborteam@gmail.com>
2025-08-09 10:51:22 +0200

    Add botanix

--- src/chains/ChainInitializer.ts
+++ src/chains/ChainInitializer.ts
@@ -4,6 +4,7 @@ import {ConfigParser, ConfigTemplate, ParsedConfig, Command} from "@atomiqlabs/s
 import {SolanaChainInitializer} from "./solana/SolanaChainInitializer";
 import {StarknetChainInitializer} from "./starknet/StarknetChainInitializer";
 import {CitreaChainInitializer} from "./evm/CitreaChainInitializer";
+import {BotanixChainInitializer} from "./evm/BotanixChainInitializer";
 
 export type ChainInitializer<T extends ChainType, C, V extends ConfigTemplate<C>> = {
     loadChain: (configuration: ParsedConfig<C, V>, bitcoinRpc: BitcoinRpc<any>, bitcoinNetwork: BitcoinNetwork) => Omit<ChainData<T>, "allowedTokens"> & {commands?: Command<any>[]},
@@ -13,5 +14,6 @@ export type ChainInitializer<T extends ChainType, C, V extends ConfigTemplate<C>
 export const RegisteredChains = {
     SOLANA: SolanaChainInitializer,
     STARKNET: StarknetChainInitializer,
-    CITREA: CitreaChainInitializer
+    CITREA: CitreaChainInitializer,
+    BOTANIX: BotanixChainInitializer
 }
---
+++ src/chains/evm/BotanixChainInitializer.ts
@@ -0,0 +1,69 @@
+import {ChainInitializer} from "../ChainInitializer";
+import {
+    numberParser,
+    objectParser,
+    stringParser,
+    enumParser
+} from "@atomiqlabs/server-base";
+import {
+    EVMFees,
+    EVMSigner,
+    initializeBotanix, BotanixChainType
+} from "@atomiqlabs/chain-evm";
+import {JsonRpcProvider} from "ethers";
+import {getEVMSigner} from "./signer/BaseEVMSigner";
+import {EVMChainEvents} from "@atomiqlabs/chain-evm/dist/evm/events/EVMChainEvents";
+
+const template = {
+    RPC_URL: stringParser(),
+    MAX_LOGS_BLOCK_RANGE: numberParser(false, 1, undefined, true),
+    MAX_FEE_GWEI: numberParser(true, 0),
+    FEE_TIP_GWEI: numberParser(true, 0, undefined, true),
+    CHAIN: enumParser(["MAINNET", "TESTNET"]),
+
+    MNEMONIC_FILE: stringParser(null, null, true),
+    PRIVKEY: stringParser(66, 66, true),
+
+    AUTHORIZATION_TIMEOUT: numberParser(false, 10, 3600, true)
+} as const;
+
+export const BotanixChainInitializer: ChainInitializer<BotanixChainType, any, typeof template> = {
+    loadChain: (configuration, bitcoinRpc, bitcoinNetwork) => {
+        const directory = process.env.STORAGE_DIR;
+
+        const provider = new JsonRpcProvider(configuration.RPC_URL);
+
+        const {chainInterface, btcRelay, swapContract, spvVaultContract} = initializeBotanix({
+            rpcUrl: provider,
+            chainType: configuration.CHAIN,
+            maxLogsBlockRange: configuration.MAX_LOGS_BLOCK_RANGE,
+            fees: new EVMFees(
+                provider,
+                BigInt(Math.floor(configuration.MAX_FEE_GWEI * 1_000_000_000)),
+                configuration.FEE_TIP_GWEI==null ? undefined : BigInt(Math.floor(configuration.FEE_TIP_GWEI * 1_000_000_000))
+            )
+        }, bitcoinRpc, bitcoinNetwork);
+
+        console.log("Init provider: ", provider);
+        const evmSigner = getEVMSigner(configuration);
+
+        const chainEvents = new EVMChainEvents(
+            directory, chainInterface, swapContract, spvVaultContract
+        );
+
+        const signer = new EVMSigner(evmSigner, evmSigner.address);
+
+        return {
+            signer,
+            swapContract,
+            chainEvents,
+            btcRelay,
+            chainInterface,
+            spvVaultContract,
+            commands: []
+        };
+    },
+    configuration: objectParser(template, (data) => {
+        if(data.MNEMONIC_FILE==null && data.PRIVKEY==null) throw new Error("Mnemonic file or explicit private key must be specified!");
+    }, true)
+};
