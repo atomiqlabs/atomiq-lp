1a5962dd179e6ca285664247cc4d7a77c7441710
adambor <adamborteam@gmail.com>
2025-10-15 23:45:35 +0200

    Add Alpen

--- src/chains/ChainInitializer.ts
+++ src/chains/ChainInitializer.ts
@@ -5,6 +5,7 @@ import {SolanaChainInitializer} from "./solana/SolanaChainInitializer";
 import {StarknetChainInitializer} from "./starknet/StarknetChainInitializer";
 import {CitreaChainInitializer} from "./evm/CitreaChainInitializer";
 import {BotanixChainInitializer} from "./evm/BotanixChainInitializer";
+import {AlpenChainInitializer} from "./evm/AlpenChainInitializer";
 
 export type ChainInitializer<T extends ChainType, C, V extends ConfigTemplate<C>> = {
     loadChain: (configuration: ParsedConfig<C, V>, bitcoinRpc: BitcoinRpc<any>, bitcoinNetwork: BitcoinNetwork) => Omit<ChainData<T>, "allowedTokens"> & {commands?: Command<any>[], minNativeBalanceReserve?: bigint},
@@ -15,5 +16,6 @@ export const RegisteredChains = {
     SOLANA: SolanaChainInitializer,
     STARKNET: StarknetChainInitializer,
     CITREA: CitreaChainInitializer,
-    BOTANIX: BotanixChainInitializer
+    BOTANIX: BotanixChainInitializer,
+    ALPEN: AlpenChainInitializer
 }
